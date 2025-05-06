7b911af751cf99a04c0e3bd9bd71b9250a91bacc
Vincenza <vinni2690@hotmail.com>
2025-05-06 16:54:47 +0200

    es-4 completato

--- src/src/esercizio4/Esercizio4.java
+++ src/src/esercizio4/Esercizio4.java
@@ -6,7 +6,14 @@ public class Esercizio4 {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("inserisci un numero");
         int num = scanner.nextInt();
+
+        while(num >= 0){
+            System.out.println(num);
+            num--;
+        }
+        System.out.println("Conto alla rovescia terminato!");
 
     }
 }
